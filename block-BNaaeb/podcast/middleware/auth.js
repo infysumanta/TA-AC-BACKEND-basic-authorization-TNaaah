b51eb23532453637f509e5e4a12386d4f968d2d7
Sumanta Kabiraj <sumantablog@gmail.com>
2022-08-24 23:18:35 +0530

    Initial Setup

---
+++ block-BNaaeb/podcast/middleware/auth.js
@@ -0,0 +1,26 @@
+const User = require("../models/User");
+
+module.exports = {
+  isUserLogged: (req, res, next) => {
+    if (req.session && req.session.userId) {
+      next();
+    } else {
+      res.redirect("/users/login");
+    }
+  },
+  userInfo: (req, res, next) => {
+    var userId = req.session && req.session.userId;
+    if (userId) {
+      User.findById(userId, "name email type subscription", (err, user) => {
+        if (err) next(err);
+        req.user = user;
+        res.locals.user = user;
+        next();
+      });
+    } else {
+      req.user = null;
+      res.locals.user = null;
+      next();
+    }
+  },
+};
